d355e0c55aad29428558b27810f220d952347d3e
hj59172507 <huaj59172507@gmail.com>
2019-02-02 00:39:44 -0500

    finsih SVM training, testing, and accuracy plot

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -1,6 +1,7 @@
 import numpy as np
 import dataProcess
 import random
+import matplotlib.pyplot as plt
 
 #calculate the cost 
 def getCost(x, y, a, b, lam):
@@ -38,11 +39,11 @@ trainFV = dataProcess.dataNormalization(trainFV)
 testFV = dataProcess.dataNormalization(testFV)
 
 #define constants for training SVM, this SVM use sign(ax + b) to make prediction
-lambdas = [0.0001, 0.001, 0.01, 0.1, 1]
+lambdas = [0.00000001, 0.000001, 0.0001, 0.0007, 1]
 aVectors = [[1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]]
 bVector = [1,1,1,1,1]
-batchSize, seasons, stepsPerSeason, accTestNumber, confirmAccSteps = 1, 50, 300, 50, 30
-tempAccs = [[],[],[],[],[]]
+batchSize, seasons, stepsPerSeason, accTestNumber, confirmAccSteps = 1, 100, 1000, 400, 30
+tempAccs, coefVectors = [[],[],[],[],[]], [[],[],[],[],[]]
 
 #split the train data to 90-10 for training and validating
 trainModelFV, testModelFV, trainModelLabels, testModelLabels = [], [], [], []
@@ -51,18 +52,53 @@ dataProcess.splitData(ptest, testModelFV, testModelLabels, trainModelFV, trainMo
 
 #loop to train models build with different lambdas
 for season in range(1, seasons+1):
-	learnRate = 100 / (season+10000)
-	trainFVDiv, testFV50, trainLabsDiv, testLabs50 = [],[], [], []
-	dataProcess.getRandomN(accTestNumber, testFV50, testLabs50, trainFVDiv, trainLabsDiv, trainModelFV, trainModelLabels)
+	learnRate = 1/(0.1*season+500)
+	trainFVDiv, testHeldout, trainLabsDiv, testHeldoutLabels = [],[], [], []
+	dataProcess.getRandomN(accTestNumber, testHeldout, testHeldoutLabels, trainFVDiv, trainLabsDiv, trainModelFV, trainModelLabels)
 	for step in range(1, stepsPerSeason+1):
 		#update each model use
 		for lam in range(0, len(lambdas)):
 			x,y = dataProcess.getRandomSample(trainFVDiv, trainLabsDiv)
 			gradiantDecent(x, y, aVectors, bVector, lambdas, learnRate, lam)
-			if(step % 30 == 0):
-				tempAccs[lam].append(getTempAcc(aVectors[lam], bVector[lam], testFV50, testLabs50))
+			if(step % confirmAccSteps == 0):
+				tempAccs[lam].append(getTempAcc(aVectors[lam], bVector[lam], testHeldout, testHeldoutLabels))
+				coefVectors[lam].append(np.sum(np.abs(aVectors[lam])))
 
-for i in range(0, len(tempAccs)):
-	size = len(tempAccs[i])
-	print(f'last ten acc: {tempAccs[i][size-10:size-1]} for lambda {lambdas[i]}')
 
+for i in range(0, len(tempaccs)):
+	x = np.linspace(0,seasons,len(tempaccs[i]))
+	plt.plot(x, tempaccs[i], label=f'lambda = {lambdas[i]}')
+plt.legend(loc='lower right')
+plt.show()
+
+for i in range(0, len(coefvectors)):
+	x = np.linspace(0,seasons,len(coefvectors[i]))
+	plt.plot(x, coefvectors[i], label=f'lambda = {lambdas[i]}')
+plt.legend(loc='lower right')
+plt.show()
+
+#train using all training set
+finalA = [1,1,1,1,1,1]
+bestLam = np.argmax(np.mean(tempAccs, axis = 1))
+finalB = 1
+outputFile = 'submission.txt'
+
+for season in range(1, seasons+1):
+	learnRate = 1/(0.01*season+200)
+	for step in range(1, stepsPerSeason+1):
+		x,y = dataProcess.getRandomSample(trainFV, trainLabels)
+		cost = getCost(x, y, finalA, finalB, bestLam)
+		if(cost != 0):
+			finalA -= np.dot(learnRate,(np.dot(finalA,bestLam)-np.dot(x,y)))
+			finalB += learnRate*y
+		else:
+			finalA -= np.dot(learnRate,np.dot(finalA,bestLam))
+
+#predict unseen data 
+with open(outputFile, 'w') as file:
+	for i in range(0, len(testFV)):
+		label = np.dot(finalA,testFV[i]) + finalB
+		if(label <= 0):
+			file.write('<=50K\n')
+		else:
+			file.write('>50K\n')
