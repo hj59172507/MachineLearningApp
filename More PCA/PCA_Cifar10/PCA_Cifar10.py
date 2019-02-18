f297090e399f47a9447e465d2d7432c9c5cfaa88
hj59172507 <huaj59172507@gmail.com>
2019-02-18 16:40:38 -0500

    finish implement PCA with first 20 componenets, generate mean class image for all 10 classes

--- More PCA/PCA_Cifar10/PCA_Cifar10.py
+++ More PCA/PCA_Cifar10/PCA_Cifar10.py
@@ -1,12 +1,17 @@
 from scipy.spatial.distance import pdist
 from sklearn.metrics import euclidean_distances
 from sklearn.decomposition import PCA
-import numpy as np
 from numpy.linalg import svd
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import csv
 
 #constants
-pComponents, categoryCount = 20, 10
+nComp, categoryCount = 20, 10
+eucFile, pcaDicFile = 'partb_distances.csv','partc_distances.csv'
 files = ['data_batch_1','data_batch_2','data_batch_3','data_batch_4','data_batch_5','test_batch']
+category = ['class1','class2','class3','class4','class5','class6','class7','class8','class9','class10']
 
 #to read data, script reference to https://www.cs.toronto.edu/~kriz/cifar.html
 def unpickle(file):
@@ -17,10 +22,10 @@ def unpickle(file):
 
 #put data into appropriate datalist according to data label
 def putIntoCategory(dataList, data):
-	arr = data[b'data']
+	arrs = data[b'data'].astype(float)
 	labels = data[b'labels']
 	for labelIndex in range(len(labels)):
-		dataList[labels[labelIndex]].append(arr[labelIndex])
+		dataList[labels[labelIndex]].append(arrs[labelIndex])
 
 #read data from each file and put into appropriate category
 def readAll(files):
@@ -34,11 +39,78 @@ def readAll(files):
 def getMeanForAll(dataList):
 	meanList = []
 	for category in dataList:
-		meanlist.append(np.mean(category, 0))
+		meanList.append(np.mean(category, 0))
 	return meanList
+
+#reconstruct a category using pca and return square different between reconstructed and original version
+def pcaRecon(pca, data, mean):
+	temp = 0
+	for d in data:
+		recon = np.dot(pca.transform(d.reshape(1,-1))[:,:nComp], pca.components_[:nComp,:]) + mean
+		temp += sum(euclidean_distances(recon.reshape(1,-1), d.reshape(1,-1)))[0]
+	return temp / len(data)
+
+#reconstruct data using svd with specified principle componenets and return average square differences between reconstructed and original version
+def getAvgSqDiff(dataList, meanList):
+	diffList = []
+	for i in range(len(dataList)):
+		pca = PCA(n_components=nComp)
+		pca.fit(dataList[i])
+		diffList.append(pcaRecon(pca, dataList[i], meanList[i]))
+	return diffList
+
+#plot the bar graph for mean different with each category
+def plotBar(List):
+	yPos = np.arange(len(category))
+	plt.bar(yPos, List, align='center')
+	plt.ylabel('mean difference')
+	plt.xticks(yPos, category)
+	plt.title('Mean difference with each Class')
+	plt.show()
+
+#plot the mean image of each class
+def plotMeanImage(meanList):
+	for x in range(len(meanList)):
+		mean = meanList[x]
+		img = []
+		for i in range(32):
+			img.append([])
+			for j in range(32):
+				img[i].append([])
+				img[i][j] = np.array([mean[i*32+j],mean[i*32+j+32*32],mean[i*32+j+32*32*2]]).astype(np.uint8)
+		plt.imshow(img)
+		plt.title('Mean image of Class' + str(x))
+		plt.show()
 
 dataList = readAll(files)
 meanList = getMeanForAll(dataList)
+#Compute mean iamge and sum square difference
+plotMeanImage(meanList)
+#diffList = getAvgSqDiff(dataList, meanList)
+#plotBar(diffList)
+
+#compute euclidean distance between all pair of class using mean image
+def getDisMatrixEuclidean(meanList):
+	eucDisMat = []
+	for i in range(len(meanList)):
+		eucDisMat.append([])
+		for j in range(len(meanList)):
+			dis = sum(euclidean_distances(meanList[i].reshape(1,-1), meanList[j].reshape(1,-1)))[0]
+			eucDisMat[i].append(dis*dis)
+	return eucDisMat
+
+#helper to write result to a file
+def writeAsCSVTo(file, data):
+	with open(eucFile, 'wb') as file:
+		fwriter = csv.writer(file,delimiter=',')
+		for row in data:
+			fwriter.writerow(row)
+
+#Compute distance martrix, perform multi-dimensional scaling and plot the first two component
+eucDisMat = getDisMatrixEuclidean(meanList)
+
+
+#Compute distance martrix define by E(A->B) = (E(A|B)+E(B|A))/2, perform multi-dimensional scaling and plot the first two component
 
 print(dataList)
 
