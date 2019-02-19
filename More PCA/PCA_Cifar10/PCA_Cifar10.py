9337ca5e35cb4ce76ca414cd7af3c68221cbd423
hj59172507 <huaj59172507@gmail.com>
2019-02-18 22:36:28 -0500

    implement multi-dimentional scaling with similarity distance matrix

--- More PCA/PCA_Cifar10/PCA_Cifar10.py
+++ More PCA/PCA_Cifar10/PCA_Cifar10.py
@@ -9,9 +9,9 @@ import csv
 
 #constants
 nComp, categoryCount, mdsComp = 20, 10, 2
-eucFile, pcaDicFile = 'partb_distances.csv','partc_distances.csv'
-files = ['data_batch_1','data_batch_2','data_batch_3','data_batch_4','data_batch_5','test_batch']
-category = ['class1','class2','class3','class4','class5','class6','class7','class8','class9','class10']
+eucFile, pcaFile = 'partb_distances.csv','partc_distances.csv'
+files = ['data_batch_1','data_batch_2','data_batch_3','data_batch_4','data_batch_5','test_batch']   
+category = ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
 
 #to read data, script reference to https://www.cs.toronto.edu/~kriz/cifar.html
 def unpickle(file):
@@ -46,8 +46,9 @@ def getMeanForAll(dataList):
 def pcaRecon(pca, data, mean):
 	temp = 0
 	for d in data:
-		recon = np.dot(pca.transform(d.reshape(1,-1))[:,:nComp], pca.components_[:nComp,:]) + mean
-		temp += sum(euclidean_distances(recon.reshape(1,-1), d.reshape(1,-1)))[0]
+		d = d.reshape(1,-1)
+		recon = np.dot(pca.transform(d)[:,:nComp], pca.components_[:nComp,:]) + mean
+		temp += np.square(sum(euclidean_distances(recon.reshape(1,-1), d)))[0]
 	return temp / len(data)
 
 #reconstruct data using svd with specified principle componenets and return average square differences between reconstructed and original version
@@ -64,7 +65,7 @@ def plotBar(List):
 	yPos = np.arange(len(category))
 	plt.bar(yPos, List, align='center')
 	plt.ylabel('mean difference')
-	plt.xticks(yPos, category)
+	plt.xticks(yPos, category, rotation = 90)
 	plt.title('Mean difference with each Class')
 	plt.show()
 
@@ -79,11 +80,12 @@ def plotMeanImage(meanList):
 				img[i].append([])
 				img[i][j] = np.array([mean[i*32+j],mean[i*32+j+32*32],mean[i*32+j+32*32*2]]).astype(np.uint8)
 		plt.imshow(img)
-		plt.title('Mean image of Class' + str(x))
+		plt.title('Mean image of Category '+str(x) + ' : ' + category[x])
 		plt.show()
 
 dataList = readAll(files)
 meanList = getMeanForAll(dataList)
+
 #Compute mean iamge and sum square difference
 #plotMeanImage(meanList)
 #diffList = getAvgSqDiff(dataList, meanList)
@@ -99,9 +101,38 @@ def getDisMatrixEuclidean(meanList):
 			eucDisMat[i].append(dis*dis)
 	return eucDisMat
 
+#compute distance using PCA, distance of A -> B denoted by E(A->B) is define as (E(A|B) + E(B|A))/2 
+#where E(A|B) is computed by using the mean of A and first 20 principal components from B to reconstruct A
+def getDisMatrixPCA(meanList, dataList):
+	pcaDisMat = []
+	for i in range(len(meanList)):
+		pcaDisMat.append([])
+		for j in range(len(meanList)):
+			pcaDisMat[i].append([])
+	for i in range(len(meanList)):
+		for j in range(i,len(meanList)):
+			if(i==j):
+				pca = PCA(n_components=nComp)
+				pca.fit(dataList[i])
+				distance = pcaRecon(pca, dataList[i], meanList[i])
+				pcaDisMat[i][j] = distance
+			else:
+				pcaI = PCA(n_components=nComp)
+				pcaJ = PCA(n_components=nComp)
+				pcaI.fit(dataList[i])
+				pcaJ.fit(dataList[j])
+				pcaI.mean_ = meanList[j]
+				pcaJ.mean_ = meanList[i]
+				tempI = pcaRecon(pcaJ, dataList[i], meanList[i])
+				tempJ = pcaRecon(pcaI, dataList[j], meanList[j])
+				distance = (tempI + tempJ)/2
+				pcaDisMat[i][j] = distance
+				pcaDisMat[j][i] = distance
+	return pcaDisMat
+
 #helper to write result to a file
 def writeAsCSVTo(file, data):
-	with open(eucFile, 'wb') as file:
+	with open(file, 'w') as file:
 		fwriter = csv.writer(file,delimiter=',')
 		for row in data:
 			fwriter.writerow(row)
@@ -123,19 +154,25 @@ def MDS(disMat):
 
 #plot a scatter plot with given title
 def scatterPlot(x, y, title):
-	plt.scatter(x, y)
+	for i in range(len(category)):
+		plt.scatter(x[i], y[i])
+		plt.text(x[i], y[i], category[i])
 	plt.xlabel("First component")
 	plt.ylabel("Second component")
 	plt.title(title)
+	plt.show()
 
 #Compute distance martrix, perform multi-dimensional scaling and plot the first two component
 eucDisMat = getDisMatrixEuclidean(meanList)
+writeAsCSVTo(eucFile, eucDisMat)
 x,y = MDS(eucDisMat)
-scatterPlot(x, y, "Scatter plot using Euclidean distance")
+scatterPlot(x, y, "MDS using Euclidean distance")
 
 #Compute distance martrix define by E(A->B) = (E(A|B)+E(B|A))/2, perform multi-dimensional scaling and plot the first two component
-
-print(dataList)
+pcaDisMat = getDisMatrixPCA(meanList, dataList)
+writeAsCSVTo(pcaFile, pcaDisMat)
+x,y = MDS(pcaDisMat)
+scatterPlot(x, y, "MDS using pca reconstruct distance")
 
 
 
