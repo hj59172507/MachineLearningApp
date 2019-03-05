05a4c8fb2b304fd5e27f68934ae19667c5eb29ca
hj59172507 <huaj59172507@gmail.com>
2019-03-05 18:52:54 -0500

    refactor getSegment method by split its responsibility into two separate method

--- Vector Quantization/ForestClassiferWithVectorQuantization/processData.py
+++ Vector Quantization/ForestClassiferWithVectorQuantization/processData.py
@@ -1,8 +1,5 @@
 import os
 
-#relative path to each class folder
-paths = ['Brush_teeth','Climb_stairs','Comb_hair','Descend_stairs','Drink_glass','Eat_meat','Eat_soup','Getup_bed','Liedown_bed','Pour_water','Sitdown_chair','Standup_chair','Use_telephone','Walk']
-
 #use paths provided to find all file names for each class
 def getDataFiles(paths):
 	classCount = len(paths)
@@ -18,22 +15,27 @@ def getDataFiles(paths):
 	return dataFiles
 
 #specify a fix length and file path for all classes, open each file and cut it into piece of segment length
-def getAllSegment(segmentLength, dataFiles):
+def getAllSegments(segmentLength, dataFiles):
 	segments = []
 	for i in range(len(dataFiles)):
 		for file in dataFiles[i]:
-			with open(file) as f:
-				lines = f.readlines();
-				size = segmentLength;
-				segment = []
-				for line in lines:
-					if(size == 0):
-						segments.append(segment)
-						segment = []
-						size = segmentLength
-					segment.extend([int(x) for x in line.split()])
-					size -= 1
+			segments.append(getSegment(file, segmentLength))
 	return segments
+
+#given a file name, return list of segments
+def getSegment(file, segmentLength):
+	segment = []
+	with open(file) as f:
+		lines = f.readlines();
+		size = segmentLength;
+		for line in lines:
+			if(size == 0):
+				segments.append(segment)
+				segment = []
+				size = segmentLength
+			segment.extend([int(x) for x in line.split()])
+			size -= 1
+	return segment
 
 #dummy varible to check result
 dataFiles = getDataFiles(paths)
