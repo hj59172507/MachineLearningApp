42381860b2ffa5d629cca13f51cea9d30df2b5fa
hj59172507 <huaj59172507@gmail.com>
2019-03-05 16:53:55 -0500

    implement method to generate fix size segment from all data files

--- Vector Quantization/ForestClassiferWithVectorQuantization/processData.py
+++ Vector Quantization/ForestClassiferWithVectorQuantization/processData.py
@@ -1,7 +1,7 @@
 import os
 
+#relative path to each class folder
 paths = ['Brush_teeth','Climb_stairs','Comb_hair','Descend_stairs','Drink_glass','Eat_meat','Eat_soup','Getup_bed','Liedown_bed','Pour_water','Sitdown_chair','Standup_chair','Use_telephone','Walk']
-
 
 #use paths provided to find all file names for each class
 def getDataFiles(paths):
@@ -14,5 +14,26 @@ def getDataFiles(paths):
 		for root, dirs, files in os.walk(paths[i]):
 			for file in files:
 				if "txt" in file:
-					dataFiles[i].add(file)
+					dataFiles[i].add(root+'/'+file)
 	return dataFiles
+
+#specify a fix length and file path for all classes, open each file and cut it into piece of segment length
+def getAllSegment(segmentLength, dataFiles):
+	segments = []
+	for i in range(len(dataFiles)):
+		for file in dataFiles[i]:
+			with open(file) as f:
+				lines = f.readlines();
+				size = segmentLength;
+				segment = []
+				for line in lines:
+					if(size == 0):
+						segments.append(segment)
+						segment = []
+						size = segmentLength
+					segment.extend([int(x) for x in line.split()])
+					size -= 1
+	return segments
+
+dataFiles = getDataFiles(paths)
+segments = getAllSegment(32,dataFiles)
