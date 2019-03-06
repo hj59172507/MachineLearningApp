8b96b7acee49fa88cade79911c30e98831dda52c
hj59172507 <huaj59172507@gmail.com>
2019-03-06 11:04:03 -0500

    implement method to calculate cluster model for the first time, then load the model afterward to avoid repeated calculation

--- Vector Quantization/ForestClassiferWithVectorQuantization/ForestClassiferWithVectorQuantization.py
+++ Vector Quantization/ForestClassiferWithVectorQuantization/ForestClassiferWithVectorQuantization.py
@@ -1,2 +1,25 @@
 import processData as pData
+import numpy as np
+import os
+from sklearn.cluster import AgglomerativeClustering
+from joblib import dump, load
 
+#load and return cluster model if already computed before, else compute, save and return the model
+def getClusterModel(clusterModelLib, allSegmentsFile, paths, segmentLength, clusterCount):
+	if os.path.isfile(clusterModelLib):
+		return load(clusterModelLib)
+	else:
+		segmentsFromAllFiles = pData.getSegmentsFromDisk(allSegmentsFile, paths, segmentLength)
+		aggCluster = AgglomerativeClustering(n_clusters = clusterCount).fit(segmentsFromAllFiles)
+		dump(aggCluster, clusterModelLib)
+		return aggCluster
+
+#relative path to each class folder
+paths = ['Brush_teeth','Climb_stairs','Comb_hair','Descend_stairs','Drink_glass','Eat_meat','Eat_soup','Getup_bed','Liedown_bed','Pour_water','Sitdown_chair','Standup_chair','Use_telephone','Walk']
+#define fix size
+segmentLength = 32
+clusterCount = 480
+allSegmentsFile, clusterModelLib = 'allSegments.npy', 'cluster.joblib'
+
+aggCluster = getClusterModel(clusterModelLib, allSegmentsFile, paths, segmentLength, clusterCount)
+print(aggCluster.labels_)
