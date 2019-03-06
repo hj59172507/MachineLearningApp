1c9c2eabc7e89336b91b0b144a34cab0463583bc
hj59172507 <huaj59172507@gmail.com>
2019-03-06 15:59:24 -0500

    implement getFeatureVector that predict a signal by cutting it into fixed size piece and predict based on previously trained k mean cluster

--- Vector Quantization/ForestClassiferWithVectorQuantization/ForestClassiferWithVectorQuantization.py
+++ Vector Quantization/ForestClassiferWithVectorQuantization/ForestClassiferWithVectorQuantization.py
@@ -1,7 +1,7 @@
 import processData as pData
 import numpy as np
 import os
-from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import KMeans
 from joblib import dump, load
 
 #load and return cluster model if already computed before, else compute, save and return the model
@@ -10,19 +10,27 @@ def getClusterModel(clusterModelLib, allSegmentsFile, dataFiles, segmentLength, 
 		return load(clusterModelLib)
 	else:
 		segmentsFromAllFiles = pData.getSegmentsFromDisk(allSegmentsFile, dataFiles, segmentLength)
-		aggCluster = AgglomerativeClustering(n_clusters = clusterCount).fit(segmentsFromAllFiles)
-		dump(aggCluster, clusterModelLib)
-		return aggCluster
+		kmeans = KMeans(n_clusters = clusterCount).fit(segmentsFromAllFiles)
+		dump(kmeans, clusterModelLib)
+		return kmeans
+
+#given a sample file, return a histogram vector from cluster model
+def getFeatureVector(kmeans, file, segmentLength):
+	fVector = [0 for i in range(kmeans.n_clusters)]
+	segments = pData.getSegment(file, segmentLength)
+	for label in kmeans.predict(segments):
+		fVector[label] += 1
+	return fVector
 
 #relative path to each class folder
 paths = ['Brush_teeth','Climb_stairs','Comb_hair','Descend_stairs','Drink_glass','Eat_meat','Eat_soup','Getup_bed','Liedown_bed','Pour_water','Sitdown_chair','Standup_chair','Use_telephone','Walk']
 #define fix size
-segmentLength, clusterCount, splitCount = 32, 480, 3
+segmentLength, clusterCount, splitCount = 32, 48, 3
 allSegmentsFile, clusterModelLib = 'allSegments.npy', 'cluster.joblib'
 
 #load cluster model
 dataFiles = pData.getDataFiles(paths)
-aggCluster = getClusterModel(clusterModelLib, allSegmentsFile, dataFiles, segmentLength, clusterCount)
+kmeans = getClusterModel(clusterModelLib, allSegmentsFile, dataFiles, segmentLength, clusterCount)
 
 #load train and test set
 splitedDataFiles = pData.splitDataFile(dataFiles, splitCount)
@@ -30,4 +38,6 @@ testSet1, trainSet1 = pData.getTrainAndTestSet(splitedDataFiles, 0)
 testSet2, trainSet2 = pData.getTrainAndTestSet(splitedDataFiles, 1)
 testSet3, trainSet3 = pData.getTrainAndTestSet(splitedDataFiles, 2)
 
-print(aggCluster.labels_)
+#find histogram for files
+hist = getFeatureVector(kmeans, testSet1[0][0][0], segmentLength)
+print(kmeans.labels_)
